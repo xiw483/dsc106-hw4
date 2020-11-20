5f850ffa2880856d073c3a61565f370c950fa66f
xiw483 <xiw483@ucsd.edu>
2020-11-19 22:01:59 -0800

    Update highchart_chart.js

--- highchart_chart.js
+++ highchart_chart.js
@@ -34,8 +34,8 @@ else {
 function getPct() {
     f = localStorage.getItem("num_fork");
     k = localStorage.getItem("num_knife");
-    if (f==0 & k== 0) {
-        return [1,1]
+    if (f == 0 & k == 0) {
+        return [1, 1]
     }
     else {
         return [Number(f) / (Number(f) + Number(k)), Number(k) / (Number(f) + Number(k))]
@@ -80,9 +80,7 @@ function renderPieChart() {
     global_pie = pie;
 }
 
-document.getElementById("order_btn").addEventListener('click', function (event) {
-    global_pie.series[0].setData([{ y: getPct()[0] }, { y: getPct()[1] }])
-})
+
 
 var globalBar;
 renderBarChart();
@@ -91,153 +89,151 @@ function pctPerSale() {
     const f = Number(global_fork_input);
     const k = Number(global_knife_input);
     return [Number(f) / (Number(f) + Number(k)), Number(k) / (Number(f) + Number(k))]
+}
 
-function renderBarChart() {
-    var bar = Highcharts.chart('bar_container', {
-        chart: { type: 'bar' },
-        title: { text: 'Number of Forks and Knives Sold per Order' },
-        xAxis: {
-            categories: ['1', '2'],
-            title: { text: 'Sales ID' }
-        },
-        yAxis: {
-            min: 0,
-            max: 1,
-            title: { text: 'Percentage Sales of Knives and Forks' }
-        },
-        legend: { reversed: true },
-        plotOptions: {
-            series: { 
-                stacking: 'normal' ,
-                dataLabels: {
-                    enabled: true,
-                    inside: true,
-                    color: 'white',
-                    format: '<b>{point.name}</b><br>{point.percentage:.1f} %',
-                    distance: -50,
-                    filter: {
-                        property: 'percentage',
-                        operator: '>',
-                        value: 4
-                    }
-                }
+    function renderBarChart() {
+        var bar = Highcharts.chart('bar_container', {
+            chart: { type: 'bar' },
+            title: { text: 'Number of Forks and Knives Sold per Order' },
+            xAxis: {
+                categories: ['1', '2'],
+                title: { text: 'Sales ID' }
             },
-            
-        },
-        series: [{
-            name: 'Knife',
-            data: [1 / 2, 2 / 6]
-        }, {
-            name: 'Fork',
-            data: [1 / 2, 4 / 6]
-        }]
-    });
-    globalBar = bar;
-}
-document.getElementById("order_btn").addEventListener('click', function (event) {
-    globalBar.xAxis[0].categories.push(id-1)
-    globalBar.series[0].addPoint(pctPerSale()[1]);
-    globalBar.series[1].addPoint(pctPerSale()[0]);
-})
+            yAxis: {
+                min: 0,
+                max: 1,
+                title: { text: 'Percentage Sales of Knives and Forks' }
+            },
+            legend: { reversed: true },
+            plotOptions: {
+                series: {
+                    stacking: 'normal',
+                    dataLabels: {
+                        enabled: true,
+                        inside: true,
+                        color: 'white',
+                        format: '<b>{point.name}</b><br>{point.percentage:.1f} %',
+                        distance: -50,
+                        filter: {
+                            property: 'percentage',
+                            operator: '>',
+                            value: 4
+                        }
+                    }
+                },
 
-
-let initialize = 1;
-function generateEntries() {
-    // Returns an orderHistory array
-    // [ID#, Date, Knife quantity, Fork quantity]
-    return [
-        [1, '10/19/2020', 1, 1, 'Paypal'],
-        [2, '10/20/2020', 2, 4, 'Visa']
-    ]
-}
-
-function tot_price(arr) {
-    return (arr[2] * KNIFE_PRICE + arr[3] * FORK_PRICE).toFixed(2);
-}
-
-function insert_data(arr) {
-    var sales = tot_price(arr);
-    var data = Array.from(arr);
-    data.splice(4, 0, "$" + sales)
-
-    num_sales += Number(sales);
-    num_knife += Number(data[2]);
-    num_fork += Number(data[3]);
-    document.getElementById("num_sales").innerHTML = num_sales.toFixed(2);
-    document.getElementById("num_knife").innerHTML = num_knife;
-    document.getElementById("num_fork").innerHTML = num_fork;
-
-    var table = document.getElementById('order_tbl').getElementsByTagName('tbody')[0];
-    var tr = table.insertRow()
-    for (let i = 0; i < data.length; i++) {
-        var cell = tr.insertCell(i);
-        cell.innerHTML = data[i];
+            },
+            series: [{
+                name: 'Knife',
+                data: [1 / 2, 2 / 6]
+            }, {
+                name: 'Fork',
+                data: [1 / 2, 4 / 6]
+            }]
+        });
+        globalBar = bar;
     }
 
-    store.orderHistory.push(arr)
-    localStorage.setItem('order_history', store.orderHistory)
-    id++;
-    localStorage.setItem('order_id', id);
-    localStorage.setItem('num_fork', num_fork)
-    localStorage.setItem('num_knife', num_knife)
-}
+    document.getElementById("order_btn").addEventListener('click', function (event) {
+        global_pie.series[0].setData([{ y: getPct()[0] }, { y: getPct()[1] }])
+        globalBar.xAxis[0].categories.push(id - 1)
+        globalBar.series[0].addPoint(pctPerSale()[1]);
+        globalBar.series[1].addPoint(pctPerSale()[0]);
+    })
 
-function check_form() {
-    const knife_input = document.getElementById("knife_input").value;
-    const fork_input = document.getElementById("fork_input").value;
-    var cansubmit = knife_input > 0 || fork_input > 0;
-    document.getElementById("order_btn").disabled = !cansubmit;
-}
+    let initialize = 1;
+    function generateEntries() {
+        // Returns an orderHistory array
+        // [ID#, Date, Knife quantity, Fork quantity]
+        return [
+            [1, '10/19/2020', 1, 1, 'Paypal'],
+            [2, '10/20/2020', 2, 4, 'Visa']
+        ]
+    }
 
-function update_form() {
-    var knife_total_price = (document.getElementById("knife_input").value * KNIFE_PRICE).toFixed(2);
-    var fork_total_price = (document.getElementById("fork_input").value * FORK_PRICE).toFixed(2);
-    document.getElementById("knife_total_price").value = knife_total_price;
-    document.getElementById("fork_total_price").value = fork_total_price;
-    document.getElementById("total_price").value = (Number(knife_total_price) + Number(fork_total_price)).toFixed(2);
-}
+    function tot_price(arr) {
+        return (arr[2] * KNIFE_PRICE + arr[3] * FORK_PRICE).toFixed(2);
+    }
 
-function order() {
-    var arr = new Array(4);
+    function insert_data(arr) {
+        var sales = tot_price(arr);
+        var data = Array.from(arr);
+        data.splice(4, 0, "$" + sales)
 
-    var order_id = localStorage.getItem("order_id");
+        num_sales += Number(sales);
+        num_knife += Number(data[2]);
+        num_fork += Number(data[3]);
+        document.getElementById("num_sales").innerHTML = num_sales.toFixed(2);
+        document.getElementById("num_knife").innerHTML = num_knife;
+        document.getElementById("num_fork").innerHTML = num_fork;
 
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0');
-    var yyyy = today.getFullYear();
-    var date = mm + '/' + dd + '/' + yyyy;
+        var table = document.getElementById('order_tbl').getElementsByTagName('tbody')[0];
+        var tr = table.insertRow()
+        for (let i = 0; i < data.length; i++) {
+            var cell = tr.insertCell(i);
+            cell.innerHTML = data[i];
+        }
 
-    const knife_input = document.getElementById("knife_input").value;
-    const fork_input = document.getElementById("fork_input").value;
+        store.orderHistory.push(arr)
+        localStorage.setItem('order_history', store.orderHistory)
+        id++;
+        localStorage.setItem('order_id', id);
+        localStorage.setItem('num_fork', num_fork)
+        localStorage.setItem('num_knife', num_knife)
+    }
 
-    global_knife_input = knife_input;
-    global_fork_input = fork_input;
+    function check_form() {
+        const knife_input = document.getElementById("knife_input").value;
+        const fork_input = document.getElementById("fork_input").value;
+        var cansubmit = knife_input > 0 || fork_input > 0;
+        document.getElementById("order_btn").disabled = !cansubmit;
+    }
 
-    var payment_method = document.getElementById("payment").value;
+    function update_form() {
+        var knife_total_price = (document.getElementById("knife_input").value * KNIFE_PRICE).toFixed(2);
+        var fork_total_price = (document.getElementById("fork_input").value * FORK_PRICE).toFixed(2);
+        document.getElementById("knife_total_price").value = knife_total_price;
+        document.getElementById("fork_total_price").value = fork_total_price;
+        document.getElementById("total_price").value = (Number(knife_total_price) + Number(fork_total_price)).toFixed(2);
+    }
 
-    arr[0] = order_id;
-    arr[1] = date;
-    arr[2] = knife_input;
-    arr[3] = fork_input;
-    arr[4] = payment_method;
+    function order() {
+        var arr = new Array(4);
 
-    insert_data(arr);
-    clear_field();
-}
+        var order_id = localStorage.getItem("order_id");
 
-function clear_field() {
-    document.getElementById("knife_input").value = 0;
-    document.getElementById("fork_input").value = 0;
-    document.getElementById("knife_total_price").value = 0.00;
-    document.getElementById("fork_total_price").value = 0.00;
-    document.getElementById("total_price").value = 0.00;
-    document.getElementById("payment").value = null;
-    document.getElementById("order_btn").disabled = "disabled";
-}
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0');
+        var yyyy = today.getFullYear();
+        var date = mm + '/' + dd + '/' + yyyy;
 
-var order_history = localStorage.getItem("order_history");
+        const knife_input = document.getElementById("knife_input").value;
+        const fork_input = document.getElementById("fork_input").value;
 
+        global_knife_input = knife_input;
+        global_fork_input = fork_input;
 
+        var payment_method = document.getElementById("payment").value;
 
+        arr[0] = order_id;
+        arr[1] = date;
+        arr[2] = knife_input;
+        arr[3] = fork_input;
+        arr[4] = payment_method;
 
+        insert_data(arr);
+        clear_field();
+    }
+
+    function clear_field() {
+        document.getElementById("knife_input").value = 0;
+        document.getElementById("fork_input").value = 0;
+        document.getElementById("knife_total_price").value = 0.00;
+        document.getElementById("fork_total_price").value = 0.00;
+        document.getElementById("total_price").value = 0.00;
+        document.getElementById("payment").value = null;
+        document.getElementById("order_btn").disabled = "disabled";
+    }
+
+    var order_history = localStorage.getItem("order_history");
